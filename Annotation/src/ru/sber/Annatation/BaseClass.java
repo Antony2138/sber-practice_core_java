d205af699b1f287050cbde2d8773815e21a8c929
Antony2138 <toxa.zag.dom@mail.ru>
2023-05-30 01:56:14 +0300

    Revert "Выполнено задание по лекции Annotation
    This reverts commit af94e22efb3255da963f1adf86e675cfc071da92.

--- Annotation/src/ru/sber/Annatation/BaseClass.java
+++
@@ -1,9 +0,0 @@
-package ru.sber.Annatation;
-
-@DayWeek(dayWeek = 1)
-
-public abstract class BaseClass {
-    protected DataContainer container;
-    public abstract void setData(String specification);
-}
-
